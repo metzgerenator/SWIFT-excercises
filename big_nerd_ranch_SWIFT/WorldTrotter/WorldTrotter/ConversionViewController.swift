134ce4bc7d1cccba1a40d6661d33362907c2e551
Michael Metzger <metzgerenator@gmail.com>
2016-02-22 23:22:16 -0500

    added chapter 4

---
+++ big_nerd_ranch_SWIFT/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Aileen Taboy on 2/22/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Double?{
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        print("Current text: \(textField.text)")
+        print("Replacement text: \(string)")
+        
+        return true
+    }
+    
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return(value - 32) * (5/9)
+        }else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+            
+            print(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func fahrenheitFieldditingchanged(textField: UITextField) {
+        
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+   
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+}
+
