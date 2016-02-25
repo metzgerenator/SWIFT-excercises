0ffe22472462485b8ca80b027730a6894a546661
Michael Metzger <metzgerenator@gmail.com>
2016-02-24 23:06:43 -0500

    finished chapter 6.  Need to complete challanges

---
+++ big_nerd_ranch_SWIFT/Chapter 6/WorldTrotter/WorldTrotter/BigNerdWeb.swift
@@ -0,0 +1,18 @@
+//
+//  BigNerdWeb.swift
+//  WorldTrotter
+//
+//  Created by Aileen Taboy on 2/24/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+
+class BigNerdWebController: UIViewController{
+    
+    var webview: WKWebView!
+    
+    
+    
+}
