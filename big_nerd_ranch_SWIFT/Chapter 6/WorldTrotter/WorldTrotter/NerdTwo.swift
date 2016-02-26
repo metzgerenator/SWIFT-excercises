6fc960349041559fefc9592cc9df8f19f600da5f
metzgerenator <metzgerenator@gmail.com>
2016-02-26 10:12:11 -0500

    finished web challange chapter 6

--- big_nerd_ranch_SWIFT/Chapter 6/WorldTrotter/WorldTrotter/NerdTwo.swift
+++ big_nerd_ranch_SWIFT/Chapter 6/WorldTrotter/WorldTrotter/NerdTwo.swift
@@ -10,14 +10,37 @@ import WebKit
 
 
 class NerdTwo: UIViewController {
-    var webView = WKWebView!()
+    
+    //WKNavigationDelegate
+    var webView = WKWebView?()
     
     override func loadView() {
-        view = webView
         
-         let url = NSURL(string: "https://www.bignerdranch.com/")
+        webView = WKWebView()
+        
+        view = webView
+//        let configuration = WKWebViewConfiguration()
+//        
+//        self.webView = WKWebView(frame: self.view.bounds, configuration: configuration)
+        
+        
+        
+        print("loading web view")
+        
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = NSURL(string: "https://www.bignerdranch.com/")
         let request = NSURLRequest(URL: url!)
-        webView.loadRequest(request)
+        print(request)
+        webView!.loadRequest(request)
+        
+        print("calling view did load")
+        
     }
     
     
