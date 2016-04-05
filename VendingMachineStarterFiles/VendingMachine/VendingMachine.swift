e1c63efd7a9ee65586b2faf2f1192fab1f364a21
Michael Metzger <metzgerenator@gmail.com>
2016-04-04 22:46:20 -0400

    added vending machine app

---
+++ VendingMachineStarterFiles/VendingMachine/VendingMachine.swift
@@ -0,0 +1,125 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Aileen Taboy on 4/4/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+// Protocols 
+
+protocol VendingMachineType {
+    var selection: [VendingSelection] { get }
+    
+    var inventory: [VendingSelection: ItemType] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: ItemType])
+    
+    func vend(selection: VendingSelection, quantity: Double) throws
+    func deposit(amount: Double)
+    
+}
+
+protocol ItemType {
+    var price: Double { get }
+    var quantity: Double { get set }
+}
+
+
+//Error Types 
+
+enum InventoryError: ErrorType {
+    case InvalidResource
+    case ConversionError
+}
+
+
+//Helper Classes 
+
+class PlistConverter{
+    class func dictionaryFromFile(resource: String, ofType type: String) throws -> [String : AnyObject] {
+        
+        guard let path = NSBundle.mainBundle().pathForResource(resource, ofType: type) else {
+            throw InventoryError.InvalidResource
+        }
+        guard let dictionary = NSDictionary(contentsOfFile: path), let castDictionary = dictionary as ? [String: AnyObject] else{
+            throw InventoryError.ConversionError
+        }
+        
+        return dictionary
+    }
+    
+    
+}
+
+
+
+
+
+//Concrete Types
+
+enum VendingSelection {
+    
+    case Soda
+    case DietSoda
+    case Chips
+    case Cookie
+    case Sandwich
+    case Wrap
+    case CandyBar
+    case PopTart
+    case Water
+    case FruitJuice
+    case SportsDrink
+    case Gum
+}
+
+
+struct VendingItem: ItemType {
+    let price: Double
+    var quantity: Double
+    
+}
+
+
+class VendingMachine: VendingMachineType {
+    let selection: [VendingSelection] = [.Soda, .DietSoda, .Chips, .Cookie, .Sandwich, .Wrap, .CandyBar, .PopTart, .Water, .FruitJuice, .SportsDrink, .Gum]
+    
+    var inventory: [VendingSelection: ItemType]
+    var amountDeposited: Double = 10.0
+    
+    required init(inventory: [VendingSelection : ItemType]) {
+        self.inventory = inventory
+    }
+    
+    func vend(selection: VendingSelection, quantity: Double) throws {
+        //add code
+    }
+    
+    func deposit(amount: Double) {
+        //add code
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
