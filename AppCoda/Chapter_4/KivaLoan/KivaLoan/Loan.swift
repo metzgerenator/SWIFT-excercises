3b4f3aab04f0a1083045781525f1923e10c795d6
metzgerenator <metzgerenator@gmail.com>
2016-04-14 16:13:10 -0400

    added AppCoda chapter 4

---
+++ AppCoda/Chapter_4/KivaLoan/KivaLoan/Loan.swift
@@ -0,0 +1,19 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Michael Metzger  on 4/14/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class Loan {
+    var name:String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
