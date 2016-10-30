381cb31b1ce2f30c1f824b9c9d5c8c67dc32fd02
Michael Metzger <metzgerenator@gmail.com>
2016-10-29 22:19:20 -0400

    added updated excercise

---
+++ faceSnap/faceSnap/MediaPickerManager.swift
@@ -0,0 +1,79 @@
+//
+//  MediaPickerManager.swift
+//  faceSnap
+//
+//  Created by Aileen Taboy on 10/29/16.
+//  Copyright © 2016 Michael Metzger . All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+
+protocol MediaPickermanagerDelegate: class {
+    
+    func mediaPickerManager(manager: MediaPickerManager, didFinishPickingImage image: UIImage)
+}
+
+class MediaPickerManager: NSObject {
+    
+    private let imagePickerController = UIImagePickerController()
+    private let presentingViewController: UIViewController
+    
+    weak var delegate: MediaPickermanagerDelegate?
+    
+    
+    init(presentingViewController: UIViewController) {
+        self.presentingViewController = presentingViewController
+         super.init()
+        
+        
+        //imagePickerController.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            imagePickerController.sourceType = .Camera
+            imagePickerController.cameraDevice = .Front
+            
+        } else {
+            
+            imagePickerController.sourceType = .PhotoLibrary
+        }
+        
+        
+        imagePickerController.mediaTypes = [kUTTypeImage as String]
+        
+    }
+    
+    
+    func presentImagePickerController(animated animated: Bool) {
+        presentingViewController.presentViewController(imagePickerController, animated: animated, completion: nil)
+        
+    }
+    
+    
+    func dismissImagePickerController(animated animated: Bool, completion: (() -> Void)) {
+        
+        imagePickerController.dismissViewControllerAnimated(animated, completion: completion)
+        
+        
+    }
+    
+    
+}
+
+
+extension MediaPickerManager: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        delegate?.mediaPickerManager(self, didFinishPickingImage: image)
+        
+    }
+    
+    
+}
+
+
+
+
+
---
+++ faceSnap/faceSnap/PhotoListController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  faceSnap
+//
+//  Created by Michael Metzger  on 10/29/16.
+//  Copyright © 2016 Michael Metzger . All rights reserved.
+//
+
+import UIKit
+
+class PhotoListController: UIViewController {
+    
+    
+    lazy var cameraButton: UIButton = {
+        
+        let button  = UIButton(type: .System)
+        
+        button.setTitle("Camera", forState: .Normal)
+        button.tintColor = .whiteColor()
+        button.backgroundColor = UIColor(red: 254/255.0, green: 123/255.0, blue: 135/255.0, alpha: 1.0)
+        
+        button.addTarget(self, action: #selector(PhotoListController.presentImagePickerController), forControlEvents: .TouchUpInside)
+        
+        
+        return button
+        
+    }()
+    
+    
+    lazy var mediaPickerManager: MediaPickerManager = {
+       let manager  = MediaPickerManager(presentingViewController: self)
+        manager.delegate = self
+        return manager
+        
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    // MARK:  - Layout
+    override func viewDidLayoutSubviews() {
+        
+        view.addSubview(cameraButton)
+        cameraButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activateConstraints([
+            cameraButton.leftAnchor.constraintEqualToAnchor(view.leftAnchor),
+            cameraButton.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor),
+            cameraButton.rightAnchor.constraintEqualToAnchor(view.rightAnchor),
+            cameraButton.heightAnchor.constraintEqualToConstant(56.0)
+            ])
+        
+    }
+    //MARK:  -Image Picker Controller
+    
+    
+    @objc private func presentImagePickerController() {
+        
+        mediaPickerManager.presentImagePickerController(animated: true)
+        
+    }
+    
+
+
+}
+
+
+//MARK:  -MediaPickerManagerDelegate 
+
+extension PhotoListController: MediaPickermanagerDelegate {
+    
+    func mediaPickerManager(manager: MediaPickerManager, didFinishPickingImage image: UIImage) {
+        
+    }
+    
+    
+}
+
